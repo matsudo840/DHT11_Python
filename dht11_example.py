04ddb13efe513d4b0154ab175f1479ccb8f388dd
Ryo Hashiguchi <ryohhhhh@gmail.com>
2019-02-27 15:17:50 +0900

    first commit

--- dht11_example.py
+++ dht11_example.py
@@ -1,15 +1,18 @@
+#!/usr/bin/python3
 import RPi.GPIO as GPIO
 import dht11
 import time
 import datetime
+import ambient
+import os
 
 # initialize GPIO
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.cleanup()
 
-# read data using pin 14
-instance = dht11.DHT11(pin=14)
+# read data using pin 18
+instance = dht11.DHT11(pin=18)
 
 while True:
     result = instance.read()
@@ -17,5 +20,5 @@ while True:
         print("Last valid input: " + str(datetime.datetime.now()))
         print("Temperature: %d C" % result.temperature)
         print("Humidity: %d %%" % result.humidity)
-
-    time.sleep(1)
+        ambi = ambient.Ambient(os.environ["AMBIENT_ID"], os.environ["AMBIENT_WTOKEN"])
+        r = ambi.send({"d1": result.temperature, "d2": result.humidity})
