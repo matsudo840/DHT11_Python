531d9ceac6ffafd2b88ba0232af0bbf908a8db04
Ryo Hashiguchi <ryohhhhh@gmail.com>
2019-02-27 23:01:10 +0900

    stop loop

--- dht11_example.py
+++ dht11_example.py
@@ -14,9 +14,8 @@ GPIO.cleanup()
 # read data using pin 18
 instance = dht11.DHT11(pin=18)
 
-while True:
-    result = instance.read()
-    if result.is_valid():
-        print('{},Temp:{},Humid:{}'.format(datetime.datetime.now(), result.temperature, result.humidity))
-        ambi = ambient.Ambient(os.environ["AMBIENT_ID"], os.environ["AMBIENT_WTOKEN"])
-        r = ambi.send({"d1": result.temperature, "d2": result.humidity})
+result = instance.read()
+if result.is_valid():
+    print('{},Temp:{},Humid:{}'.format(datetime.datetime.now(), result.temperature, result.humidity))
+    ambi = ambient.Ambient(os.environ["AMBIENT_ID"], os.environ["AMBIENT_WTOKEN"])
+    r = ambi.send({"d1": result.temperature, "d2": result.humidity})
