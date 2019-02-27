9e94bd38834bb0deb57871cbcda441691041be89
Ryo Hashiguchi <ryohhhhh@gmail.com>
2019-02-27 23:00:40 +0900

    change message

--- dht11_example.py
+++ dht11_example.py
@@ -17,8 +17,6 @@ instance = dht11.DHT11(pin=18)
 while True:
     result = instance.read()
     if result.is_valid():
-        print("Last valid input: " + str(datetime.datetime.now()))
-        print("Temperature: %d C" % result.temperature)
-        print("Humidity: %d %%" % result.humidity)
+        print('{},Temp:{},Humid:{}'.format(datetime.datetime.now(), result.temperature, result.humidity))
         ambi = ambient.Ambient(os.environ["AMBIENT_ID"], os.environ["AMBIENT_WTOKEN"])
         r = ambi.send({"d1": result.temperature, "d2": result.humidity})
