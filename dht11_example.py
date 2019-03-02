4d64cc33720573b3c4b9ce0521ec37a7872713d9
Ryo Hashiguchi <ryohhhhh@gmail.com>
2019-03-02 20:47:15 +0900

    logging

--- dht11_example.py
+++ dht11_example.py
@@ -11,13 +11,14 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.cleanup()
 
-# read data using pin 18
-instance = dht11.DHT11(pin=18)
+# read data using pin 14
+instance = dht11.DHT11(pin=14)
 
 while True:
     result = instance.read()
     if result.is_valid():
-        print('{},Temp:{},Humid:{}'.format(datetime.datetime.now(), result.temperature, result.humidity))
+        with open('dht11_example.log', 'a', encoding='utf-8') as f:
+            f.write('{},Temp:{},Humid:{}\n'.format(datetime.datetime.now(), result.temperature, result.humidity))
         ambi = ambient.Ambient(os.environ["AMBIENT_ID"], os.environ["AMBIENT_WTOKEN"])
         r = ambi.send({"d1": result.temperature, "d2": result.humidity})
         break
